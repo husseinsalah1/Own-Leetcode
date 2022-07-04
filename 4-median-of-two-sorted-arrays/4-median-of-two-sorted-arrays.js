54efca84c01d4b3284c04207c7f8827ad8003617
Hussein Salah <48605045+husseinsalah1@users.noreply.github.com>
2022-07-04 03:39:23 +0200

    Time: 145 ms (68.33%), Space: 48.7 MB (21.90%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -5,19 +5,19 @@
  */
 var findMedianSortedArrays = function(nums1, nums2) {
 var medianResult;
-  var MergedArrayWithRemoveDuplicate = [...nums1, ...nums2].sort(
+  var MergedArray = [...nums1, ...nums2].sort(
     (a, b) => a - b
   );
-  if (MergedArrayWithRemoveDuplicate.length % 2 === 0) {
-    var median_1 = MergedArrayWithRemoveDuplicate.length / 2 - 1;
-    var median_2 = MergedArrayWithRemoveDuplicate.length / 2;
+  if (MergedArray.length % 2 === 0) {
+    var median_1 = MergedArray.length / 2 - 1;
+    var median_2 = MergedArray.length / 2;
     medianResult =
-      (MergedArrayWithRemoveDuplicate[median_1] +
-        MergedArrayWithRemoveDuplicate[median_2]) /
+      (MergedArray[median_1] +
+        MergedArray[median_2]) /
       2;
   } else {
-    var medianIndex = MergedArrayWithRemoveDuplicate.length / 2 - 0.5;
-    medianResult = MergedArrayWithRemoveDuplicate[medianIndex];
+    var medianIndex = MergedArray.length / 2 - 0.5;
+    medianResult = MergedArray[medianIndex];
   }
 
   return medianResult;
