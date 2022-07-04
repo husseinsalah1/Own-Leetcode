b538af037da6645d75c26135e45aa7dfe1051dd6
Hussein Salah <48605045+husseinsalah1@users.noreply.github.com>
2022-07-04 10:59:04 +0200

    Time: 430 ms (19.50%), Space: 52.8 MB (11.08%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,27 +3,30 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    var arr = [];
-  var sub = '';
-  if (s.length === 1) {
-    return 1;
+  if (s.length === 0 || s.length === 1) {
+    return s.length;
   }
-  for (var i = 0; i < s.length; i++) {
-    sub = s[i];
-    for (var j = i + 1; j < s.length; j++) {
-      if (sub.includes(s[j])) {
-        arr[sub] = sub.length;
-        sub = '';
+  var start = 0;
+  var arr = [];
+  var str = '';
+  while (start < s.length) {
+    str = s[start];
+    var end = start + 1;
+    while (end < s.length) {
+      if (str.includes(s[end])) {
+        arr[str] = str.length;
+        end = end + 1;
         break;
       } else {
-        sub = sub + s[j];
+        str = str + s[end];
+        end = end + 1;
         continue;
       }
     }
-    arr[sub] = sub.length;
+    arr[str] = str.length;
+    start = start + 1;
   }
-  arr[sub] = sub.length;
+  arr[str] = str.length;
 
   return Object.values(arr).sort((a, b) => b - a)[0];
-
 };
