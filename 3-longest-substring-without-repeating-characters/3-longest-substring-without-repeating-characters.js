3f0bd0fe1c6d51dcd88788b5022c74c1f53b4a78
Hussein Salah <48605045+husseinsalah1@users.noreply.github.com>
2022-07-04 10:55:25 +0200

    Time: 594 ms (14.32%), Space: 53.2 MB (10.64%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  if (s.length === 0 || s.length === 1) {
+    return s.length;
+  }
+  var start = 0;
+  var arr = [];
+  var str = '';
+  while (start < s.length) {
+    str = s[start];
+    var end = start + 1;
+    while (end < s.length) {
+      if (str.includes(s[end])) {
+        arr[str] = str.length;
+        end = end + 1;
+        break;
+      } else {
+        str = str + s[end];
+        end = end + 1;
+        continue;
+      }
+    }
+    arr[str] = str.length;
+    start = start + 1;
+  }
+  arr[str] = str.length;
+
+  return Object.values(arr).sort((a, b) => b - a)[0];
+};
